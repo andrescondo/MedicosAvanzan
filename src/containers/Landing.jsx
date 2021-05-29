56d21353c17c85538676b8d6e5e03316df97a14a
andrescondo <1dkevinandres.b@gmail.com>
2021-05-28 19:24:30 -0500

    added hero model, message and image

--- src/containers/Landing.jsx
+++ src/containers/Landing.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import '../styles/components/landing.css'
+import hero from '../img/header.png'
+
+
 const Landing = () => {
   return(
     <div className="Landing">
-      
+      <section className="hero">
+        <div className="hero-title">
+          <h3>
+            Encuentra el mejor sitio para: <br />
+            • Almacenar datos de tus pacientes de manera segura <br />
+            • Agendar citas y <br />
+            • llevar un control de ellos
+          </h3>
+
+        </div>
+        <figure>
+          <img src={hero} alt="Foto Header"/>
+        </figure>
+      </section>
+      <section className="content">
+
+      </section>
+      <section className="price">
+
+      </section>
     </div>
   )
 }
